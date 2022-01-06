feb5c79935e5cdcfda5407196620bf2c93abaa67
Alejandro Gutiérrez <alejandrodevs@gmail.com>
2022-01-06 11:46:10 -0600

    Rename iv_defined? method to ivar_defined?

--- lib/sphene/attribute.rb
+++ lib/sphene/attribute.rb
@@ -9,24 +9,24 @@ module Sphene
     end
 
     def value
-      return default if !iv_defined?(:@value_before_cast)
-      return @value if iv_defined?(:@value)
+      return default if !ivar_defined?(:@value_before_cast)
+      return @value if ivar_defined?(:@value)
       @value = type.cast(value_to_cast)
     end
 
     def value=(value)
-      remove_instance_variable(:@value) if iv_defined?(:@value)
+      remove_instance_variable(:@value) if ivar_defined?(:@value)
       @value_before_cast = value
     end
 
     private
 
     def value_to_cast
-      iv_defined?(:@value_before_cast) ?
+      ivar_defined?(:@value_before_cast) ?
         @value_before_cast : default
     end
 
-    def iv_defined?(name)
+    def ivar_defined?(name)
       instance_variable_defined?(name)
     end
   end
